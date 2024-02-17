e76abbfacc00d74a7e4c01a7ccf1c93e32897dfb
Vikas Soni <techvik678@gmail.com>
2024-02-18 03:32:53 +0530

    first commit

---
+++ src/components/TodoFiltered.js
@@ -0,0 +1,55 @@
+import React from 'react'
+
+function TodoFiltered(props) {
+  return (
+    <div className="todo-app">
+        <h1>Completed Items</h1>
+        {props.todos.map((todo)=>{
+
+      if(todo.complete===true){
+        return(
+            <>
+           
+            <div
+            className={todo.isComplete ? "todo-row complete" : "todo-row"}
+        
+          >
+            {todo.text}
+            </div>
+            </>
+        )
+      }
+
+      
+    })}
+
+
+<h1>Inompleted Items</h1>
+        {props.todos.map((todo)=>{
+
+      if(todo.complete===false){
+        return(
+            <>
+           
+            <div
+            className={todo.isComplete ? "todo-row complete" : "todo-row"}
+        
+          >
+            {todo.text}
+            </div>
+            </>
+        )
+      }
+
+      
+    })}
+
+
+
+
+
+ </div>
+  )
+}
+
+export default TodoFiltered
---
+++ src/components/TodoList.js
@@ -0,0 +1,92 @@
+import React, { useState ,useEffect} from "react";
+import TodoForm from "./TodoForm";
+import Todo from "./Todo";
+import TodoFiltered from "./TodoFiltered";
+
+function TodoList() {
+  const [todos, setTodos] = useState([localStorage.getItem('array')]);
+//   const [completedtodos, setCompletedTodos] = useState([]);
+//   const [incompletedtodos, setIncompletedTodos] = useState([]);
+const [filter,setFilter]=useState(false)
+
+// useEffect(() => {
+//     const storedData = localStorage.getItem('array');
+//     if (storedData) {
+//       setTodos(JSON.parse(storedData));
+//     }
+//   }, []);
+
+  const addTodo = (todo) => {
+    if (!todo.text || /^\s*$/.test(todo.text)) {
+      return;
+    }
+    const newTodos = [todo, ...todos];
+    
+    setTodos(newTodos);
+    console.log(todos);
+    
+   
+   
+  };
+
+
+  
+
+  const removeTodo= id =>{
+    const removeArr=[...todos].filter(todo => todo.id !== id)
+    setTodos(removeArr);
+  }
+
+  const updateTodo=(todoId,  newValue)=>{
+    if (!newValue.text || /^\s*$/.test(newValue.text)) {
+        return;
+      }
+      setTodos(prev => prev.map(item =>(item.id===todoId ? newValue : item)))
+
+  }
+
+  const completeTodo = (id) => {
+    let updatedTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        todo.isComplete = !todo.isComplete;
+        todo.complete=!todo.complete
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+    // console.log(todos); 
+  };
+
+const handleFilter=()=>{
+setFilter(true) 
+//    todos.map((todo)=>{
+//         if(todo.complete===true){
+//             const newCompletedTodos = [todo, ...todos];
+//             setCompletedTodos(newCompletedTodos)
+//         }
+//         if(todo.complete===false){
+//             const newIncompletedTodos = [todo, ...todos];
+//             setIncompletedTodos(newIncompletedTodos)
+//         }
+//     })
+}
+  if(filter===true){
+    return(
+        <div>
+      <h1>Todo App</h1>
+      {/* <TodoFiltered completed={completedtodos} incompleted={incompletedtodos}></TodoFiltered> */}
+      <TodoFiltered todos={todos}></TodoFiltered>
+      {/* <Todo todos={todos} completeTodo={completeTodo} removeTodo={removeTodo} updateTodo={updateTodo}/> */}
+    </div> 
+    )
+  }
+  return (
+    <div>
+      <h1>Add SOmething in Your List!!</h1>
+      <TodoForm onSubmit={addTodo} handleFilter={handleFilter}/>
+      <Todo todos={todos} completeTodo={completeTodo} removeTodo={removeTodo} updateTodo={updateTodo}/>
+    </div>
+  );
+}
+
+export default TodoList;
